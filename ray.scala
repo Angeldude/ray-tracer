cf0933bb303518752fad43fa2d83fa3820168979
Angel Vanegas <angelman@gmail.com>
2017-05-10 12:01:06 -0400

    changed method to look more like ray definition formula

--- ray.scala
+++ ray.scala
@@ -1,6 +1,6 @@
 import io.StdIn._
 
-/* Ray definition: r(t) = r0 + t * direction, t >= 0
+/*
   intersection by solving for the t that satisfies a condition
   sphere: (r(t)-n)*(r(t)-n) = radius^2
   plane: r(t)* n = d
@@ -8,24 +8,26 @@ import io.StdIn._
 */
 
 class Point3D(val x:Int, val y:Int, val z:Int){
-  def times(num:Int)={
+  def `*`(num:Int)={
     new Point3D(x*num, y*num, z*num)
   }
 
-  def add(point:Point3D)={
+  def `+`(point:Point3D)={
     new Point3D(x+point.x, y+point.y, z+point.z)
   }
 }
-class Ray(startPoint:Point3D, direction:Point3D){
-  def ray(t:Int)={
-    startPoint.add(direction.times(t))
+
+// Ray definition: r(t) = r0 + (rD * t), t >= 0
+class Ray(r0:Point3D, rD:Point3D){
+  def r(t:Int)={
+    r0 `+` (rD `*` t)
   }
 }
 val point = new Point3D(0,0,0)
 val dir = new Point3D(2,3,1)
-val r = new Ray(point, dir)
+val ray = new Ray(point, dir)
 
-for(n <- Range(0,10)){
-  var temp:Point3D = r.ray(n)
+for(n <- (0 to 10)){
+  var temp:Point3D = ray.r(n)
   println(temp.x, temp.y, temp.z)
 }
