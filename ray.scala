7a772b443d352d1de1427c7bbe3c9c5463277e02
Angel Vanegas <angelman@gmail.com>
2017-05-15 01:24:09 -0400

    sphere class up and running. Dunno if not normalizing vector is giving me huge points in sphere

--- ray.scala
+++ ray.scala
@@ -1,9 +1,5 @@
 import io.StdIn._
 
-/*
-  sphere: (r(t)-n)*(r(t)-n) = radius^2
-*/
-
 class Point3D(val x:Double, val y:Double, val z:Double){
   def `+`(num: Vector[Double]) = {
     new Point3D(x+num(0), y+num(1), z+num(2))
@@ -17,8 +13,8 @@ class Point3D(val x:Double, val y:Double, val z:Double){
     x*dot(0) + y*dot(1) + z*dot(2)
   }
 
-  def normal(point:Point3D) = {
-    Vector(x-point.x, y-point.y, z-point.z)
+  def sub(point:Point3D) = {
+    Vector(point.x-x, point.y-y, point.z-z)
   }
 }
 
@@ -58,15 +54,40 @@ class Plane(n:Vector[Double], d:Double){
    }
 }
 
+// sphere: (r(t)-n)*(r(t)-n) = radius^2
 class Sphere(center:Point3D, radius:Double){
-  def surfaceN(ray:Ray) = {
-    center.normal(ray.r0)
+  def eVector(ray:Ray) = {
+    center.sub(ray.r0)
+  }
+
+  def intersectSphere(ray:Ray) = {
+    val rayVector = Vector(ray.r0.x, ray.r0.y, ray.r0.z)
+    val centerVector = Vector(center.x, center.y, center.z)
+    val a = ray.rD.map(i => math.pow(i,2)).sum
+    val b = ray.rD.zip(eVector(ray)).map(i => 2 * i._1 * i._2).sum
+    val c = centerVector.map(i => math.pow(i,2)).sum +
+    rayVector.map(i => math.pow(i,2)).sum +
+    (-2 * (center `*` rayVector)) - math.pow(radius,2)
+
+    val delta = b*b - (4 * a * c)
+    if(delta < 0)
+      "There is no intersection."
+    else {
+      val t0 = (-b - math.sqrt(delta))/2*a
+      val t1 = (-b + math.sqrt(delta))/2*a
+      val t = Vector(t0,t1).min
+      val point = ray.r(t)
+      "There is an intersection at t = " + t.toString + "\n" +
+      "Giving us the point at " + (point.x.floatValue, point.y.floatValue, point.z.floatValue)
+    }
   }
 }
 
-val ray = new Ray(new Point3D(0,0,0), Vector(1,1,1))
-val plane = new Plane(Vector(1,2,3), 6)
-println(plane.intersectPlane(ray))
+val ray = new Ray(new Point3D(1,2,2), Vector(1,1,1))
+// val plane = new Plane(Vector(1,2,3), 6)
+val sphere = new Sphere(new Point3D(6,7,7), 0.5)
+// println(plane.intersectPlane(ray))
+println(sphere.intersectSphere(ray))
 
 // println("Enter 3 numbers, new line each, to represent the start point of\na Ray: ")
 // val x0 = readDouble()
