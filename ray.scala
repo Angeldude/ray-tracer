f39f65fea2a662d862539204744c8d046934b744
Angel Vanegas <angelman@gmail.com>
2017-05-17 22:16:40 -0400

    attempted to refactor and make easier to read all the calculations and formulas. Also made the point class print a nice string

--- ray.scala
+++ ray.scala
@@ -1,6 +1,14 @@
 import io.StdIn._
 
 class Point3D(val x:Double, val y:Double, val z:Double){
+  override def toString = {
+    val xF = x.floatValue
+    val yF = y.floatValue
+    val zF = z.floatValue
+    s"($xF, $yF, $zF)"
+
+  }
+
   def `+`(num: Vector[Double]) = {
     new Point3D(x+num(0), y+num(1), z+num(2))
   }
@@ -38,19 +46,19 @@ class Ray(val r0:Point3D, val rD:Vector[Double]){
 
 // plane: r(t)* n = d
 class Plane(n:Vector[Double], d:Double){
-  def intersectPlane(r:Ray) = {
-    val denominator = r.rD.zip(n).map(i => i._1 * i._2).sum
-    val numerator =  d - (r.r0 `*` n)
-    if(denominator == 0)
-      "Ray is parallel to plane,\nNo intersection"
-    else if(numerator/denominator  < 0)
-      "t is out of range, no intersection"
-    else {
-      val t = numerator/denominator
-      val point = r.r(t)
-        "There is intersection at t = " + t.toString + "\n" +
-        "Giving us the point at " + (point.x.floatValue, point.y.floatValue, point.z.floatValue)
-      }
+  def tPlane(r:Ray) = {
+    val ratio = fraction(r)
+
+    if(ratio._2 == 0)
+      0.0
+    else
+      ratio._1/ratio._2
+   }
+
+  private def fraction(r:Ray) = {
+     val denominator = r.rD.zip(n).map(i => i._1 * i._2).sum
+     val numerator =  d - (r.r0 `*` n)
+     (numerator, denominator)
    }
 }
 
@@ -60,34 +68,50 @@ class Sphere(center:Point3D, radius:Double){
     center.sub(ray.r0)
   }
 
-  def intersectSphere(ray:Ray) = {
+  def tSphere(ray:Ray) = {
+    val calculated = calculate
+    val delta = calculated._1
+    val a = calculated._2
+    val b = calculated._3
+
+    if(delta < 0)
+      delta
+    else {
+      val t0 = quadratic("sub", a, b, delta)
+      val t1 = quadratic("add", a, b, delta)
+      Vector(t0,t1).min
+    }
+  }
+
+  private def calculate ={
     val rayVector = Vector(ray.r0.x, ray.r0.y, ray.r0.z)
+
     val centerVector = Vector(center.x, center.y, center.z)
+
     val a = ray.rD.map(i => math.pow(i,2)).sum
+
     val b = ray.rD.zip(eVector(ray)).map(i => 2 * i._1 * i._2).sum
+
     val c = centerVector.map(i => math.pow(i,2)).sum +
     rayVector.map(i => math.pow(i,2)).sum +
     (-2 * (center `*` rayVector)) - math.pow(radius,2)
 
-    val delta = b*b - (4 * a * c)
-    if(delta < 0)
-      "There is no intersection."
-    else {
-      val t0 = (-b - math.sqrt(delta))/(2*a)
-      val t1 = (-b + math.sqrt(delta))/(2*a)
-      val t = Vector(t0,t1).min
-      val point = ray.r(t)
-      "There is an intersection at t = " + t.toString + "\n" +
-      "Giving us the point at " + (point.x.floatValue, point.y.floatValue, point.z.floatValue)
-    }
+    (b*b - (4 * a * c), a, b)
+  }
+
+  private def quadratic(operator:String, a:Double, b:Double, del:Double) = {
+    if(operator == "add")
+      (-b + math.sqrt(del))/(2*a)
+    else
+      (-b - math.sqrt(del))/(2*a)
   }
 }
 
 val ray = new Ray(new Point3D(1,2,2), Vector(1,1,1))
-// val plane = new Plane(Vector(1,2,3), 6)
+val plane = new Plane(Vector(1,2,3), 6)
 val sphere = new Sphere(new Point3D(6,7,7), 1)
-// println(plane.intersectPlane(ray))
-println(sphere.intersectSphere(ray))
+println(ray.r(sphere.tSphere(ray)))
+println(ray.r(plane.tPlane(ray)))
 
 // println("Enter 3 numbers, new line each, to represent the start point of\na Ray: ")
 // val x0 = readDouble()
